eb21bc18a9ea1b6f0fcdab146730ede305c545b3
rojasdev <r.rojas@usls.edu.ph>
2024-05-08 09:17:57 +0800

    ok

--- index.php
+++ index.php
@@ -2,11 +2,11 @@ Go to https://devlab.helioho.st to test.
 
 <br/>
 Database 
-<quote>
+<blockquote>
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
-</quote>
+</blockquote>
 
