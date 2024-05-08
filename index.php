062626cf755a09be19ec53f42e468ae9c45be121
rojasdev <r.rojas@usls.edu.ph>
2024-05-08 09:16:56 +0800

    ok

---
+++ index.php
@@ -0,0 +1,12 @@
+Go to https://devlab.helioho.st to test.
+
+<br/>
+Database 
+<quote>
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+</quote>
+
